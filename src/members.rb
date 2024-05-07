6e59b8cdd5da634377dbbcb83bb1ab5f5a894550
yoshihara88 <165636289+yoshihara88@users.noreply.github.com>
2024-05-07 14:35:29 +0900

    Create members.rb

---
+++ src/members.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+# Copyright: (C) 2020 iCub Tech Facility - Istituto Italiano di Tecnologia
+# Authors: Ugo Pattacini <ugo.pattacini@iit.it>
+
+
+#########################################################################################
+# deps
+require 'octokit'
+require 'yaml'
+require './helpers'
+
+existing_members = YAML.load_file('existing_members.yml')
+new_member = { name: 'New Member', age: 25, role: 'Developer' }
+existing_members << new_member
+File.open('updated_members.yml', 'w') { |file| file.write(existing_members.to_yaml) }
